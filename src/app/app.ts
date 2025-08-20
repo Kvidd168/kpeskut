a6400d73f4ef4ecd05149865503e66384188152b
Kvidd168 <sovandavid98@gmail,com>
2025-08-20 23:56:49 +0700

    fifth

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('kikilu');
+export class App {  
+  
 }
